8e4ae9cd194e8929e05407fc950dabd2109603a1
starchild92 <lapv1992@gmail.com>
2015-08-27 17:41:23 -0430

    Página Inicial

--- app/cache/dev/twig/6/3/635677bac68bf4e764e132c9cb4e46e7c237c21a7ecd1790064a69d025aba1b6.php
+++ app/cache/dev/twig/6/3/635677bac68bf4e764e132c9cb4e46e7c237c21a7ecd1790064a69d025aba1b6.php
@@ -30,83 +30,191 @@ class __TwigTemplate_635677bac68bf4e764e132c9cb4e46e7c237c21a7ecd1790064a69d025a
     public function block_body($context, array $blocks = array())
     {
         // line 4
-        echo "    <div class=\"container\">
+        echo "\t<div class=\"navbar-wrapper\">
+\t\t<div class=\"container\">
 
-      <!-- Static navbar -->
-      <nav class=\"navbar navbar-default\">
-        <div class=\"container-fluid\">
-          <div class=\"navbar-header\">
-            <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\" aria-controls=\"navbar\">
-              <span class=\"sr-only\">Toggle navigation</span>
-              <span class=\"icon-bar\"></span>
-              <span class=\"icon-bar\"></span>
-              <span class=\"icon-bar\"></span>
-            </button>
-            <a class=\"navbar-brand\" href=\"#\">Project name</a>
-          </div>
-          <div id=\"navbar\" class=\"navbar-collapse collapse\">
-            <ul class=\"nav navbar-nav\">
-              <li class=\"active\"><a href=\"#\">Home</a></li>
-              <li><a href=\"#\">About</a></li>
-              <li><a href=\"#\">Contact</a></li>
-              <li class=\"dropdown\">
-                <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></a>
-                <ul class=\"dropdown-menu\">
-                  <li><a href=\"#\">Action</a></li>
-                  <li><a href=\"#\">Another action</a></li>
-                  <li><a href=\"#\">Something else here</a></li>
-                  <li role=\"separator\" class=\"divider\"></li>
-                  <li class=\"dropdown-header\">Nav header</li>
-                  <li><a href=\"#\">Separated link</a></li>
-                  <li><a href=\"#\">One more separated link</a></li>
-                </ul>
-              </li>
-            </ul>
-            <ul class=\"nav navbar-nav navbar-right\">
-              <li class=\"active\"><a href=\"./\">Default <span class=\"sr-only\">(current)</span></a></li>
-              <li><a href=\"../navbar-static-top/\">Static top</a></li>
-              <li><a href=\"../navbar-fixed-top/\">Fixed top</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-      </nav>
+\t\t<nav class=\"navbar navbar-inverse navbar-static-top\">
+\t\t\t<div class=\"container\">
+\t\t\t\t<div class=\"navbar-header\">
+\t\t\t\t\t<button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\" aria-controls=\"navbar\">
+\t\t\t\t\t\t<span class=\"sr-only\">Toggle navigation</span>
+\t\t\t\t\t\t<span class=\"icon-bar\"></span>
+\t\t\t\t\t\t<span class=\"icon-bar\"></span>
+\t\t\t\t\t\t<span class=\"icon-bar\"></span>
+\t\t\t\t\t</button>
+\t\t\t\t\t<a class=\"navbar-brand\" href=\"#\">Project name</a>
+\t\t\t\t</div>
+\t\t\t\t<div id=\"navbar\" class=\"navbar-collapse collapse\">
+\t\t\t\t\t<ul class=\"nav navbar-nav\">
+\t\t\t\t\t\t<li class=\"active\"><a href=\"#\">Home</a></li>
+\t\t\t\t\t\t<li><a href=\"#about\">About</a></li>
+\t\t\t\t\t\t<li><a href=\"#contact\">Contact</a></li>
+\t\t\t\t\t\t<li class=\"dropdown\">
+\t\t\t\t\t\t\t<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></a>
+\t\t\t\t\t\t\t<ul class=\"dropdown-menu\">
+\t\t\t\t\t\t\t\t<li><a href=\"#\">Action</a></li>
+\t\t\t\t\t\t\t\t<li><a href=\"#\">Another action</a></li>
+\t\t\t\t\t\t\t\t<li><a href=\"#\">Something else here</a></li>
+\t\t\t\t\t\t\t\t<li role=\"separator\" class=\"divider\"></li>
+\t\t\t\t\t\t\t\t<li class=\"dropdown-header\">Nav header</li>
+\t\t\t\t\t\t\t\t<li><a href=\"#\">Separated link</a></li>
+\t\t\t\t\t\t\t\t<li><a href=\"#\">One more separated link</a></li>
+\t\t\t\t\t\t\t</ul>
+\t\t\t\t\t\t</li>
+\t\t\t\t\t</ul>
+\t\t\t\t</div>
+\t\t\t</div>
+\t\t</nav>
 
-      <!-- Main component for a primary marketing message or call to action -->
-      <div class=\"jumbotron\">
-        <h1>Navbar example</h1>
-        <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-        <p>
-          <a class=\"btn btn-lg btn-primary\" href=\"../../components/#navbar\" role=\"button\">View navbar docs &raquo;</a>
-        </p>
-      </div>
+\t\t</div>
+\t</div>
 
-    </div> <!-- /container -->
+\t<!-- Carousel
+\t================================================== -->
+\t<div id=\"myCarousel\" class=\"carousel slide\" data-ride=\"carousel\">
+\t\t<!-- Indicators -->
+\t\t<ol class=\"carousel-indicators\">
+\t\t\t<li data-target=\"#myCarousel\" data-slide-to=\"0\" class=\"active\"></li>
+\t\t\t<li data-target=\"#myCarousel\" data-slide-to=\"1\"></li>
+\t\t\t<li data-target=\"#myCarousel\" data-slide-to=\"2\"></li>
+\t\t\t<li data-target=\"#myCarousel\" data-slide-to=\"3\"></li>
+\t\t</ol>
+\t\t<div class=\"carousel-inner\" role=\"listbox\">
+\t\t\t<div class=\"item active\">
+\t\t\t\t<img class=\"first-slide\" src=\"";
+        // line 55
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/lihotel/images/big_4.jpg"), "html", null, true);
+        echo "\" alt=\"First slide\">
+\t\t\t\t<div class=\"container\">
+\t\t\t\t\t<div class=\"carousel-caption\">
+\t\t\t\t\t\t<h1>Example headline.</h1>
+\t\t\t\t\t\t<p>Note: If you're viewing this page via a <code>file://</code> URL, the \"next\" and \"previous\" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+\t\t\t\t\t\t<p><a class=\"btn btn-lg btn-primary\" href=\"#\" role=\"button\">Sign up today</a></p>
+\t\t\t\t\t</div>
+\t\t\t\t</div>
+\t\t\t</div>
+\t\t\t<div class=\"item\">
+\t\t\t\t<img class=\"second-slide\" src=\"";
+        // line 65
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/lihotel/images/big_1.jpg"), "html", null, true);
+        echo "\" alt=\"Second slide\">
+\t\t\t\t<div class=\"container\">
+\t\t\t\t\t<div class=\"carousel-caption\">
+\t\t\t\t\t\t<h1>Another example headline.</h1>
+\t\t\t\t\t\t<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+\t\t\t\t\t\t<p><a class=\"btn btn-lg btn-primary\" href=\"#\" role=\"button\">Learn more</a></p>
+\t\t\t\t\t</div>
+\t\t\t\t</div>
+\t\t\t</div>
+\t\t\t<div class=\"item\">
+\t\t\t\t<img class=\"third-slide\" src=\"";
+        // line 75
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/lihotel/images/big_2.jpg"), "html", null, true);
+        echo "\" alt=\"Third slide\">
+\t\t\t\t<div class=\"container\">
+\t\t\t\t\t<div class=\"carousel-caption\">
+\t\t\t\t\t\t<h1>One more for good measure.</h1>
+\t\t\t\t\t\t<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+\t\t\t\t\t\t<p><a class=\"btn btn-lg btn-primary\" href=\"#\" role=\"button\">Browse gallery</a></p>
+\t\t\t\t\t</div>
+\t\t\t\t</div>
+\t\t\t</div>
+\t\t\t<div class=\"item\">
+\t\t\t\t<img class=\"third-slide\" src=\"";
+        // line 85
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/lihotel/images/big_3.jpg"), "html", null, true);
+        echo "\" alt=\"Fourth slide\">
+\t\t\t\t<div class=\"container\">
+\t\t\t\t\t<div class=\"carousel-caption\">
+\t\t\t\t\t\t<h1>One more for good measure.</h1>
+\t\t\t\t\t\t<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+\t\t\t\t\t\t<p><a class=\"btn btn-lg btn-primary\" href=\"#\" role=\"button\">Browse gallery</a></p>
+\t\t\t\t\t</div>
+\t\t\t\t</div>
+\t\t\t</div
+\t\t</div>
+\t\t<a class=\"left carousel-control\" href=\"#myCarousel\" role=\"button\" data-slide=\"prev\">
+\t\t\t<span class=\"glyphicon glyphicon-chevron-left\" aria-hidden=\"true\"></span>
+\t\t\t<span class=\"sr-only\">Previous</span>
+\t\t</a>
+\t\t<a class=\"right carousel-control\" href=\"#myCarousel\" role=\"button\" data-slide=\"next\">
+\t\t\t<span class=\"glyphicon glyphicon-chevron-right\" aria-hidden=\"true\"></span>
+\t\t\t<span class=\"sr-only\">Next</span>
+\t\t</a>
+\t</div><!-- /.carousel -->
+
+\t<div class=\"container marketing\">
+
+\t\t<!-- START THE FEATURETTES -->
+
+\t\t<hr class=\"featurette-divider-little\">
+
+\t\t<div class=\"row featurette\">
+\t\t\t<div class=\"col-md-7\">
+\t\t\t\t<h2 class=\"featurette-heading\">First featurette heading. <span class=\"text-muted\">It'll blow your mind.</span></h2>
+\t\t\t\t<p class=\"lead\">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+\t\t\t</div>
+\t\t\t<div class=\"col-md-5\">
+\t\t\t\t<img class=\"featurette-image img-responsive center-block\" data-src=\"holder.js/500x500/auto\" alt=\"Generic placeholder image\" src=\"";
+        // line 117
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/lihotel/images/image_1.jpeg"), "html", null, true);
+        echo "\">
+\t\t\t</div>
+\t\t</div>
+
+\t\t<hr class=\"featurette-divider\">
+
+\t\t<div class=\"row featurette\">
+\t\t\t<div class=\"col-md-7 col-md-push-5\">
+\t\t\t\t<h2 class=\"featurette-heading\">Oh yeah, it's that good. <span class=\"text-muted\">See for yourself.</span></h2>
+\t\t\t\t<p class=\"lead\">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+\t\t\t</div>
+\t\t\t<div class=\"col-md-5 col-md-pull-7\">
+\t\t\t\t<img class=\"featurette-image img-responsive center-block\" data-src=\"holder.js/500x500/auto\" alt=\"Generic placeholder image\">
+\t\t\t</div>
+\t\t</div>
+
+\t\t<hr class=\"featurette-divider\">
+
+\t\t<div class=\"row featurette\">
+\t\t\t<div class=\"col-md-7\">
+\t\t\t\t<h2 class=\"featurette-heading\">And lastly, this one. <span class=\"text-muted\">Checkmate.</span></h2>
+\t\t\t\t<p class=\"lead\">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+\t\t\t</div>
+\t\t\t<div class=\"col-md-5\">
+\t\t\t\t<img class=\"featurette-image img-responsive center-block\" data-src=\"holder.js/500x500/auto\" alt=\"Generic placeholder image\">
+\t\t\t</div>
+\t\t</div>
+
+\t\t<hr class=\"featurette-divider\">
+\t\t<!-- /END THE FEATURETTES -->
+\t</div>
 ";
     }
 
-    // line 57
+    // line 150
     public function block_stylesheets($context, array $blocks = array())
     {
-        // line 58
-        echo "    <link rel=\"stylesheet\" type=\"text/css\" href=\"";
+        // line 151
+        echo "\t<link rel=\"stylesheet\" type=\"text/css\" href=\"";
         echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/lihotel/css/bootstrap.css"), "html", null, true);
         echo "\">
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"";
-        // line 59
-        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/lihotel/css/navbar.css"), "html", null, true);
+\t<link rel=\"stylesheet\" type=\"text/css\" href=\"";
+        // line 152
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/lihotel/css/carousel.css"), "html", null, true);
         echo "\">
 ";
     }
 
-    // line 61
+    // line 154
     public function block_javascripts($context, array $blocks = array())
     {
-        // line 62
-        echo "    <script src=\"";
+        // line 155
+        echo "\t<script src=\"";
         echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/lihotel/js/jquery.min.js"), "html", null, true);
         echo "\"></script>
-    <script src=\"";
-        // line 63
+\t<script src=\"";
+        // line 156
         echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/lihotel/js/bootstrap.min.js"), "html", null, true);
         echo "\"></script>
 ";
@@ -124,6 +232,6 @@ class __TwigTemplate_635677bac68bf4e764e132c9cb4e46e7c237c21a7ecd1790064a69d025a
 
     public function getDebugInfo()
     {
-        return array (  110 => 63,  105 => 62,  102 => 61,  96 => 59,  91 => 58,  88 => 57,  33 => 4,  30 => 3,  11 => 1,);
+        return array (  218 => 156,  213 => 155,  210 => 154,  204 => 152,  199 => 151,  196 => 150,  160 => 117,  125 => 85,  112 => 75,  99 => 65,  86 => 55,  33 => 4,  30 => 3,  11 => 1,);
     }
 }
